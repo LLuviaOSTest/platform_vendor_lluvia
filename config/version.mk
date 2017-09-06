58071dba09493953cc5851a22f513216583759a4
Raleigh Matlock <raleighman2@gmail.com>
2017-09-07 01:33:26 +0530

    vendor: config: Add LLuvia versioning
    Signed-off-by: Shubham Singh <coolsks94@gmail.com>
    Signed-off-by: Sebastian <kjjjnob.seba@gmail.com>
    Signed-off-by: Anush02198 <Anush.4376@gmail.com>

---
+++ config/version.mk
@@ -0,0 +1,27 @@
+# Copyright (C) 2019 LLuviaOS
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+# LLuviaOS versioning
+LLUVIA_VERSION = 5.0
+
+LLUVIA_MOD_VERSION := LLuviaOS-$(YODITA_VERSION)-$(shell date -u +%Y%m%d-%H%M)
+
+PRODUCT_PROPERTY_OVERRIDES += \
+  ro.lluvia.version=$(LLUVIA_VERSION) \
+  ro.modversion=$(LLUVIA_MOD_VERSION)
+
+YODITA_DISPLAY_VERSION := $(YODITA_VERSION)
+
+PRODUCT_PROPERTY_OVERRIDES += \
+  ro.yodita.display.version=$(YODITA_DISPLAY_VERSION)
