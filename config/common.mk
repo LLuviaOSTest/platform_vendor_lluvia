LLuviaOSTest/platform_vendor_lluvia_2

vendor: Remove sysinit
Signed-off-by: Sebastian <kjjjnob.seba@gmail.com>

config/common.mk
@@ -19,8 +19,7 @@ PRODUCT_PROPERTY_OVERRIDES += \
     ro.build.selinux=1
 
 PRODUCT_COPY_FILES += \
-    vendor/yodita/prebuilt/common/etc/init.d/00banner:system/etc/init.d/00banner \
-    vendor/yodita/prebuilt/common/bin/sysinit:system/bin/sysinit
+    vendor/yodita/prebuilt/common/etc/init.d/00banner:system/etc/init.d/00banner
 
 # Copy all Yodita specific init rc files
 $(foreach f,$(wildcard vendor/yodita/prebuilt/common/etc/init/*.rc),\

LLuviaOSTest/platform_vendor_lluvia

Signed-off-by: Anush02198 <Anush.4376@gmail.com>

config/common.mk
@@ -19,8 +19,7 @@ PRODUCT_PROPERTY_OVERRIDES += \
     ro.build.selinux=1
 
 PRODUCT_COPY_FILES += \
-    vendor/lluvia/prebuilt/common/etc/init.d/00banner:system/etc/init.d/00banner \
-    vendor/lluvia/prebuilt/common/bin/sysinit:system/bin/sysinit
+    vendor/lluvia/prebuilt/common/etc/init.d/00banner:system/etc/init.d/00banner
 
 # Copy all LLuvia specific init rc files
 $(foreach f,$(wildcard vendor/lluvia/prebuilt/common/etc/init/*.rc),\