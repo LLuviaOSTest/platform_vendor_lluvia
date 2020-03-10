LLuviaOSTest/platform_vendor_lluvia_2

vendor: Dynamically generate kernel headers using aosp generator
Add a soong vendor plugin for kernel config variables so we can use
these in go.

Change-Id: Id31f2be8fcc5aba2d965dbe815edaaf1d28279c6
Signed-off-by: Sebastian <kjjjnob.seba@gmail.com>

build/soong/generator/generator.go
@@ -213,10 +213,13 @@ func (g *Module) GenerateAndroidBuildActions(ctx android.ModuleContext) {
 	depRoot := String(g.properties.Dep_root)
 	if depRoot == "" {
 		depRoot = ctx.ModuleDir()
+	} else {
+		depRoot = yoditaExpandVariables(ctx, depRoot)
 	}
 
 	// Glob dep_files property
 	for _, dep_file := range g.properties.Dep_files {
+		dep_file = yoditaExpandVariables(ctx, dep_file)
 		globPath := filepath.Join(depRoot, dep_file)
 		paths, err := ctx.GlobWithDeps(globPath, nil)
 		if err != nil {
@@ -228,7 +231,7 @@ func (g *Module) GenerateAndroidBuildActions(ctx android.ModuleContext) {
 		}
 	}
 
-	cmd := String(g.properties.Cmd)
+	cmd := yoditaExpandVariables(ctx, String(g.properties.Cmd))
 
 	rawCommand, err := android.Expand(cmd, func(name string) (string, error) {
 		switch name {

build/soong/generator/variables.go
@@ -1,0 +1,28 @@
+package generator
+
+import (
+	"fmt"
+
+	"android/soong/android"
+)
+
+func yoditaExpandVariables(ctx android.ModuleContext, in string) string {
+	yoditaVars := ctx.Config().VendorConfig("yoditaVarsPlugin")
+
+	out, err := android.Expand(in, func(name string) (string, error) {
+		if yoditaVars.IsSet(name) {
+			return yoditaVars.String(name), nil
+		}
+		// This variable is not for us, restore what the original
+		// variable string will have looked like for an Expand
+		// that comes later.
+		return fmt.Sprintf("$(%s)", name), nil
+	})
+
+	if err != nil {
+		ctx.PropertyErrorf("%s: %s", in, err.Error())
+		return ""
+	}
+
+	return out
+}

LLuviaOSTest/platform_vendor_lluvia

Signed-off-by: Anush02198 <Anush.4376@gmail.com>

build/soong/generator/generator.go
@@ -213,10 +213,13 @@ func (g *Module) GenerateAndroidBuildActions(ctx android.ModuleContext) {
 	depRoot := String(g.properties.Dep_root)
 	if depRoot == "" {
 		depRoot = ctx.ModuleDir()
+	} else {
+		depRoot = lluviaExpandVariables(ctx, depRoot)
 	}
 
 	// Glob dep_files property
 	for _, dep_file := range g.properties.Dep_files {
+		dep_file = lluviaExpandVariables(ctx, dep_file)
 		globPath := filepath.Join(depRoot, dep_file)
 		paths, err := ctx.GlobWithDeps(globPath, nil)
 		if err != nil {
@@ -228,7 +231,7 @@ func (g *Module) GenerateAndroidBuildActions(ctx android.ModuleContext) {
 		}
 	}
 
-	cmd := String(g.properties.Cmd)
+	cmd := lluviaExpandVariables(ctx, String(g.properties.Cmd))
 
 	rawCommand, err := android.Expand(cmd, func(name string) (string, error) {
 		switch name {

build/soong/generator/variables.go
@@ -1,0 +1,28 @@
+package generator
+
+import (
+	"fmt"
+
+	"android/soong/android"
+)
+
+func lluviaExpandVariables(ctx android.ModuleContext, in string) string {
+	lluviaVars := ctx.Config().VendorConfig("yoditaVarsPlugin")
+
+	out, err := android.Expand(in, func(name string) (string, error) {
+		if lluviaVars.IsSet(name) {
+			return lluviaVars.String(name), nil
+		}
+		// This variable is not for us, restore what the original
+		// variable string will have looked like for an Expand
+		// that comes later.
+		return fmt.Sprintf("$(%s)", name), nil
+	})
+
+	if err != nil {
+		ctx.PropertyErrorf("%s: %s", in, err.Error())
+		return ""
+	}
+
+	return out
+}