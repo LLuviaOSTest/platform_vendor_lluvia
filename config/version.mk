LLuviaOSTest/platform_vendor_lluvia_2

vendor: Add ROM's custom fingerprint
Signed-off-by: Shubham Singh <coolsks94@gmail.com>
Signed-off-by: Sebastian <kjjjnob.seba@gmail.com>

config/version.mk
@@ -15,7 +15,11 @@
 # Yodita versioning
 YODITA_VERSION = 1.0
 
-YODITA_MOD_VERSION := Yodita-$(YODITA_VERSION)-$(shell date -u +%Y%m%d-%H%M)
+TARGET_PRODUCT_SHORT := $(subst yodita_,,$(YODITA_BUILD))
+
+YODITA_BUILD_DATE := $(shell date -u +%Y%m%d-%H%M)
+YODITA_MOD_VERSION := Yodita-$(YODITA_VERSION)-$(YODITA_BUILD_DATE)
+YODITA_FINGERPRINT := Yodita/$(YODITA_VERSION)/$(PLATFORM_VERSION)/$(TARGET_PRODUCT_SHORT)
 
 PRODUCT_SYSTEM_DEFAULT_PROPERTIES += \
   ro.yodita.version=$(YODITA_VERSION) \
@@ -24,4 +28,5 @@ PRODUCT_SYSTEM_DEFAULT_PROPERTIES += \
 YODITA_DISPLAY_VERSION := $(YODITA_VERSION)
 
 PRODUCT_SYSTEM_DEFAULT_PROPERTIES += \
-  ro.yodita.display.version=$(YODITA_DISPLAY_VERSION)
+  ro.yodita.display.version=$(YODITA_DISPLAY_VERSION) \
+  ro.yodita.fingerprint=$(YODITA_FINGERPRINT)

LLuviaOSTest/platform_vendor_lluvia

Signed-off-by: Anush02198 <Anush.4376@gmail.com>

config/version.mk
@@ -15,7 +15,11 @@
 # LLuviaOS versioning
 LLUVIA_VERSION = 5.0
 
-LLUVIA_MOD_VERSION := LLuviaOS-$(LLUVIA_VERSION)-$(shell date -u +%Y%m%d-%H%M)
+TARGET_PRODUCT_SHORT := $(subst lluvia_,,$(LLUVIA_BUILD))
+
+LLUVIA_BUILD_DATE := $(shell date -u +%Y%m%d-%H%M)
+LLUVIA_MOD_VERSION := -$(LLUVIA_VERSION)-$(LLUVIA_BUILD_DATE)
+YODITA_FINGERPRINT := LLUVIA/$(LLUVIA_VERSION)/$(PLATFORM_VERSION)/$(TARGET_PRODUCT_SHORT)
 
 PRODUCT_SYSTEM_DEFAULT_PROPERTIES += \
   ro.lluvia.version=$(LLUVIA_VERSION) \
@@ -24,4 +28,5 @@ PRODUCT_SYSTEM_DEFAULT_PROPERTIES += \
 LLUVIA_DISPLAY_VERSION := $(LLUVIA_VERSION)
 
 PRODUCT_SYSTEM_DEFAULT_PROPERTIES += \
-  ro.lluvia.display.version=$(LLUVIA_DISPLAY_VERSION)
+  ro.lluvia.display.version=$(LLUVIA_DISPLAY_VERSION) \
+  ro.lluvia.fingerprint=$(LLUVIA_FINGERPRINT)