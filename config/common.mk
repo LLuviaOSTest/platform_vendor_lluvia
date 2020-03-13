LLuviaOSTest/platform_vendor_lluvia_2

vendor: prebuilt: Re-enable and update LC to q_pre_alpha_2599
[ @AgentFabulous | POSP ]
- Update for Q

Change-Id: I64f1708a3e609885695ce1491a66e0f36112075a
Signed-off-by: Sebita <kjjjnob.seba@gmail.com>

config/common.mk
@@ -28,6 +28,9 @@ PRODUCT_PACKAGE_OVERLAYS += vendor/yodita/overlay/dictionaries
 # Common overlays
 PRODUCT_PACKAGE_OVERLAYS += vendor/yodita/overlay/common
 
+# Lawnchair overlays
+PRODUCT_PACKAGE_OVERLAYS += vendor/yodita/overlay/lawnchair
+
 # Backup Tool
 PRODUCT_COPY_FILES += \
     vendor/yodita/prebuilt/common/bin/backuptool.sh:install/bin/backuptool.sh \
@@ -60,11 +63,13 @@ endif
 # Sysconfigs
 PRODUCT_COPY_FILES += \
     vendor/yodita/prebuilt/common/etc/sysconfig/yodita-power-whitelist.xml:$(TARGET_COPY_OUT_SYSTEM)/etc/sysconfig/yodita-power-whitelist.xml \
-    vendor/yodita/prebuilt/common/etc/sysconfig/dialer_experience.xml:$(TARGET_COPY_OUT_SYSTEM)/etc/sysconfig/dialer_experience.xml
+    vendor/yodita/prebuilt/common/etc/sysconfig/dialer_experience.xml:$(TARGET_COPY_OUT_SYSTEM)/etc/sysconfig/dialer_experience.xml \
+    vendor/yodita/prebuilt/common/etc/sysconfig/lawnchair-hiddenapi-package-whitelist.xml:$(TARGET_COPY_OUT_SYSTEM)/etc/sysconfig/lawnchair-hiddenapi-package-whitelist.xml
 
 # Permissions
 PRODUCT_COPY_FILES += \
-    vendor/yodita/prebuilt/common/etc/permissions/privapp-permissions-elgoog.xml:$(TARGET_COPY_OUT_SYSTEM)/etc/permissions/privapp-permissions-elgoog.xml
+    vendor/yodita/prebuilt/common/etc/permissions/privapp-permissions-elgoog.xml:$(TARGET_COPY_OUT_SYSTEM)/etc/permissions/privapp-permissions-elgoog.xml \
+    vendor/yodita/prebuilt/common/etc/permissions/privapp-permissions-lawnchair.xml:$(TARGET_COPY_OUT_SYSTEM)/etc/permissions/privapp-permissions-lawnchair.xml
 
 # Do not include art debug targets
 PRODUCT_ART_TARGET_INCLUDE_DEBUG_BUILD := false

LLuviaOSTest/platform_vendor_lluvia

Signed-off-by: Anush02198 <Anush.4376@gmail.com>

config/common.mk
@@ -28,6 +28,9 @@ PRODUCT_PACKAGE_OVERLAYS += vendor/lluvia/overlay/dictionaries
 # Common overlays
 PRODUCT_PACKAGE_OVERLAYS += vendor/lluvia/overlay/common
 
+# Lawnchair overlays
+PRODUCT_PACKAGE_OVERLAYS += vendor/lluvia/overlay/lawnchair
+
 # Backup Tool
 PRODUCT_COPY_FILES += \
     vendor/lluvia/prebuilt/common/bin/backuptool.sh:install/bin/backuptool.sh \
@@ -73,6 +76,9 @@ PRODUCT_COPY_FILES += \
 # Permissions
 PRODUCT_COPY_FILES += \
     vendor/lluvia/prebuilt/common/etc/permissions/privapp-permissions-elgoog.xml:$(TARGET_COPY_OUT_SYSTEM)/etc/permissions/privapp-permissions-elgoog.xml
+    vendor/lluvia/prebuilt/common/etc/sysconfig/yodita-power-whitelist.xml:$(TARGET_COPY_OUT_SYSTEM)/etc/sysconfig/lluvia-power-whitelist.xml \
+    vendor/lluvia/prebuilt/common/etc/sysconfig/dialer_experience.xml:$(TARGET_COPY_OUT_SYSTEM)/etc/sysconfig/dialer_experience.xml \
+    vendor/lluvia/prebuilt/common/etc/sysconfig/lawnchair-hiddenapi-package-whitelist.xml:$(TARGET_COPY_OUT_SYSTEM)/etc/sysconfig/lawnchair-hiddenapi-package-whitelist.xml
 
 # Do not include art debug targets
 PRODUCT_ART_TARGET_INCLUDE_DEBUG_BUILD := false