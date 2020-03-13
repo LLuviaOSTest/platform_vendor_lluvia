LLuviaOSTest/platform_vendor_lluvia_2

vendor: Backup script update
- Bring inline with los backup script

Change-Id: If139dda6a52ac1d0f0f421c54873e127ad070899
Signed-off-by: Josh Fox (XlxFoXxlX) <joshfox87@gmail.com>
Signed-off-by: Sebastian <kjjjnob.seba@gmail.com>
Signed-off-by: Sebita <kjjjnob.seba@gmail.com>

config/common.mk
@@ -13,6 +13,7 @@ PRODUCT_COPY_FILES += \
     vendor/yodita/prebuilt/common/bin/backuptool.sh:install/bin/backuptool.sh \
     vendor/yodita/prebuilt/common/bin/backuptool.functions:install/bin/backuptool.functions \
     vendor/yodita/prebuilt/common/bin/50-base.sh:system/addon.d/50-base.sh \
+    vendor/yodita/prebuilt/common/bin/blacklist:system/addon.d/blacklist
 
 PRODUCT_SYSTEM_DEFAULT_PROPERTIES += \
     keyguard.no_require_sim=true \

LLuviaOSTest/platform_vendor_lluvia

Signed-off-by: Anush02198 <Anush.4376@gmail.com>

config/common.mk
@@ -13,6 +13,7 @@ PRODUCT_COPY_FILES += \
     vendor/lluvia/prebuilt/common/bin/backuptool.sh:install/bin/backuptool.sh \
     vendor/lluvia/prebuilt/common/bin/backuptool.functions:install/bin/backuptool.functions \
     vendor/lluvia/prebuilt/common/bin/50-base.sh:system/addon.d/50-base.sh \
+    vendor/lluvia/prebuilt/common/bin/blacklist:system/addon.d/blacklist
 
 PRODUCT_SYSTEM_DEFAULT_PROPERTIES += \
     keyguard.no_require_sim=true \